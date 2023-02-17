bc25d688777bc11d9c6105821057841d5753b7d9
ASalem404 <70176109+ASalem404@users.noreply.github.com>
2023-02-17 11:45:02 -0800

    update docker hub repo

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('ahmedas144/jenkins-learn-example')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
